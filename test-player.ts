e872021a52301d2ddc3300b6baf0e104f6111595
quantavil <quantavil@gmail.com>
2026-05-12 12:50:00 +0530

    chore: remove temporary test files

--- test-player.ts
+++
@@ -1,6 +0,0 @@
-import { player } from './src/lib/stores/player.svelte.ts';
-
-const track = { ytMusicId: '123', title: 'Test', artist: 'Test' };
-player.playTrack(track, [track]);
-console.log('visible:', player.visible);
-console.log('queue:', player.queue.length);
